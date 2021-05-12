aad558c66f3c1bedc25be761d77725aa4f8e43cf
Luis Fernando Félix Mata <51014165+LuisFerUACH@users.noreply.github.com>
2021-05-12 11:28:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return{
+      usuarios: []
+    }
+  },
+  created(){
+    this.obtenerUsuarios()
+  },
+  methods: {
+    obtenerUsuarios(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response -> response.json())
+        .then(users =>{
+          this.usuarios = users
+        })
+    }
+  },
+
+})
+
+const vm = app.mount('#app')
